b6ce364a52af7868ed4bbaebb846e8c327f82a9a
Foong <meeztix425@gmail.com>
2019-09-23 11:47:15 +0800

    added scene for prototype building placement mechanic

---
+++ Assets/BuildManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager instance;
+    
+    void Awake()
+    {
+        if(instance != null)
+        {
+            // if there is already an instance of BuildManager
+            return;
+        }
+        instance = this;
+    }
+
+    [SerializeField] GameObject defaultBuilding;
+
+    void Start()
+    {
+        buildingChoice = defaultBuilding;
+    }
+
+    private GameObject buildingChoice;
+
+    public GameObject getBuildingChoice()
+    {
+        return buildingChoice;
+    }
+}
---
+++ Assets/Node.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    [SerializeField] private Color hoverColor;
+    [SerializeField] private Vector3 positionOffset;
+
+    private GameObject building;
+    
+    private Renderer rend;
+    private Color startColor;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+
+    void OnMouseEnter()
+    {
+       rend.material.color = hoverColor;
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+
+    void OnMouseDown()
+    {
+        if(building != null)
+        {
+            // if there is already a building on it
+            return;
+        }
+
+        GameObject buildingChoice = BuildManager.instance.getBuildingChoice();
+
+        building = (GameObject)Instantiate(buildingChoice, transform.position + positionOffset, transform.rotation);
+    }
+}
