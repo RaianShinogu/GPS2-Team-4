22d40d64798a4b29f036d82389cea35cc460b588
Ryan <mingguang.lin@gmail.com>
2019-09-24 02:18:59 +0800

    Shooting

---
+++ Assets/RyanTest/Scripts/EnemyRyan.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+public class EnemyRyan : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Transform target;
+    private int wavepointindex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = Waypoints.points[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if(Vector3.Distance(transform.position, target.position) <= 0.2f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    private void GetNextWaypoint()
+    {
+        if(wavepointindex >= Waypoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        wavepointindex++;
+        target = Waypoints.points[wavepointindex];
+    }
+}
---
+++ Assets/RyanTest/Scripts/WaveSpawnRyan.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaveSpawnRyan : MonoBehaviour
+{
+    public Transform enemyPrefab;
+
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 5f;
+    private float countdown = 2f;
+
+    //private int waveIndex = 0;
+    private int waveNumber = 1;
+
+    void Update ()
+    {
+        if(countdown <= 0f)
+        {
+            //StartCoroutine(SpawnWave());
+            SpawnWave();
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+    }
+
+    /*IEnumerator SpawnWave()
+    {
+        //waveIndex++;
+        for (int i=0; i< waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }*/
+
+    void SpawnWave()
+    {
+        for (int i = 0; i < waveNumber; i++)
+        {
+            SpawnEnemy();
+        }
+        //waveNumber++;
+
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
