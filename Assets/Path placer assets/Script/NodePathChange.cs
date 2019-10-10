50bd40d03ad1d1f236ef617ef2928905f72332fd
kahou0120424 <0120424@kdu-online.com>
2019-10-10 13:37:04 +0800

    added more code

--- Assets/Path placer assets/Script/NodePathChange.cs
+++ Assets/Path placer assets/Script/NodePathChange.cs
@@ -9,68 +9,49 @@ public class NodePathChange : MonoBehaviour
     NodePathManager nodePathManager;
     Vector3 originalPos;
     Vector3 instantiatePos;
-   
+
     public int pathID;
+    private int lastPathID;
     bool changedPath = false;
     bool isPathStage = true;
-     public bool allowPath = false;
+    public bool allowPath = false;
     UIPath uiPath;
 
+    private string VerticalPath = "Vertical Path";
+    private string HorizontalPath = "Horizontal Path";
+    private string turnLeftPath = "Turn Left";
+    private string turnRightPath = "Turn Right";
+    private string inverseTurnRIghtPath = "Inverse Turn Right";
+    private string inverseTurnLeftPath = "Inverse Turn Left";
 
+    RaycastHit backHit;
+    RaycastHit leftHit;
+    RaycastHit rightHit;
+    RaycastHit forwardHit;
 
-    /*void Update()
+    void Update()
     {
 
-        RaycastHit backHit;
-        RaycastHit leftHit;
-        RaycastHit rightHit;
-        RaycastHit forwardHit;
-        Debug.DrawRay(transform.position + Vector3.up, transform.TransformDirection(Vector3.back) * 1.5f, Color.red); //(back of the object)
-        Debug.DrawRay(transform.position + Vector3.up, transform.TransformDirection(Vector3.left) * 1.5f, Color.red); //(left of the object)
-        Debug.DrawRay(transform.position + Vector3.up, transform.TransformDirection(Vector3.right) * 1.5f, Color.red);//(right of the object)
-        Debug.DrawRay(transform.position + Vector3.up, transform.TransformDirection(Vector3.forward) * 1.5f, Color.red);//(forward of the object)
+        
+        //Debug.DrawRay(transform.position + Vector3.up, transform.TransformDirection(Vector3.back) * 1.5f, Color.red); //(back of the object)
+        Debug.DrawRay(transform.position + Vector3.up, transform.TransformDirection(Vector3.left) * 1.3f, Color.red); //(left of the object)
+        //Debug.DrawRay(transform.position + Vector3.up, transform.TransformDirection(Vector3.right) * 1.5f, Color.red);//(right of the object)
+        //Debug.DrawRay(transform.position + Vector3.up, transform.TransformDirection(Vector3.forward) * 1.5f, Color.red);//(forward of the object)
         
         // X axis path
-        if(nodePathManager.pathID != 2)
+        if(nodePathManager.pathID != lastPathID)
         {
             allowPath = false;
         }
-        if (Physics.Raycast(transform.position + Vector3.up, transform.TransformDirection(Vector3.left), out leftHit, 1.5f) )
-        {
-            
-            if (leftHit.transform.CompareTag("Enemy"))
-            {
-                //Debug.Log("Hello");
-                if(nodePathManager.pathID == 2)
-                {
-                    allowPath = true;
-                }
-               
-                
-            }
-        }
 
-        else if (Physics.Raycast(transform.position + Vector3.up, transform.TransformDirection(Vector3.right), out rightHit, 1.5f))
-        {
-
-            if (rightHit.transform.CompareTag("Enemy"))
-            {
-                //Debug.Log("Hello");
-                //if(nodePathManager.pathID == 2)
-                //{
-                allowPath = true;
-                //}
-
-
-            }
-        }
+        xAxisPath();
+        yAxisPath();
 
 
 
 
 
-
-    }*/
+    }
     private void Awake()
     {
         originalPos = this.transform.position;
@@ -85,18 +66,19 @@ public class NodePathChange : MonoBehaviour
 
        if (Input.GetMouseButtonDown(0) && !changedPath)
        {
-            //if(allowPath == true)
-            //{
+            if(allowPath == true)
+            {
                 if (nodePathManager.pathID > 3)
                 {
                     Instantiate(nodePathManager.currentPathChose, instantiatePos, Quaternion.Euler(new Vector3(0, 180, 0)));
                     changedPath = true;
+                    
                 }
                 else
 
                     Instantiate(nodePathManager.currentPathChose, instantiatePos, Quaternion.identity);
                 changedPath = true;
-           //}
+           }
                 
        }
             
@@ -109,5 +91,142 @@ public class NodePathChange : MonoBehaviour
     }
 
     
-   
+   void xAxisPath()
+    {
+        
+        if (Physics.Raycast(transform.position + Vector3.up, transform.TransformDirection(Vector3.left), out leftHit, 1.3f))
+        {
+
+            if ( leftHit.transform.CompareTag(VerticalPath) || leftHit.transform.CompareTag("Enemy"))
+            {
+                //Debug.Log("Hello");
+                if (nodePathManager.pathID == 2)
+                {
+                    allowPath = true;
+                    lastPathID = nodePathManager.pathID;
+                }
+
+
+            }
+
+            else if (Physics.Raycast(transform.position + Vector3.up, transform.TransformDirection(Vector3.left), out leftHit, 1.3f))
+            {
+
+                if (leftHit.transform.CompareTag(inverseTurnLeftPath))
+                {
+                    //Debug.Log("Hello");
+                    if (nodePathManager.pathID == 2)
+                    {
+                        allowPath = true;
+                        lastPathID = nodePathManager.pathID;
+                    }
+
+
+                }
+            }
+
+            else if (Physics.Raycast(transform.position + Vector3.up, transform.TransformDirection(Vector3.left), out leftHit, 1.5f))
+            {
+
+                if (leftHit.transform.CompareTag(inverseTurnRIghtPath))
+                {
+                    //Debug.Log("Hello");
+                    if (nodePathManager.pathID == 2)
+                    {
+                        allowPath = true;
+                        lastPathID = nodePathManager.pathID;
+                    }
+
+
+                }
+            }
+
+            else if (Physics.Raycast(transform.position + Vector3.up, transform.TransformDirection(Vector3.right), out rightHit, 1.3f))
+            {
+
+                if (rightHit.transform.CompareTag(VerticalPath))
+                {
+                    //Debug.Log("Hello");
+
+                    if (nodePathManager.pathID == 2)
+                    {
+                        allowPath = true;
+                        lastPathID = nodePathManager.pathID;
+                    }
+
+
+                }
+            }
+
+            else if (Physics.Raycast(transform.position + Vector3.up, transform.TransformDirection(Vector3.right), out rightHit, 1.3f))
+            {
+
+                if (rightHit.transform.CompareTag(turnRightPath))
+                {
+                    //Debug.Log("Hello");
+                    if(nodePathManager.pathID == 2)
+                    {
+                         allowPath = true;
+                         lastPathID = nodePathManager.pathID;
+                    }
+
+
+                }
+            }
+
+            else if (Physics.Raycast(transform.position + Vector3.up, transform.TransformDirection(Vector3.right), out rightHit, 1.3f))
+            {
+
+                if (rightHit.transform.CompareTag(turnLeftPath))
+                {
+                    //Debug.Log("Hello");
+                    if (nodePathManager.pathID == 2)
+                    {
+                        allowPath = true;
+                        lastPathID = nodePathManager.pathID;
+                    }
+
+
+                }
+            }
+        }
+
+        
+
+        }
+   void yAxisPath()
+    {
+       
+        if (Physics.Raycast(transform.position + Vector3.up, transform.TransformDirection(Vector3.forward), out forwardHit, 1.5f))
+        {
+
+            if (forwardHit.transform.CompareTag(inverseTurnLeftPath))
+            {
+                //Debug.Log("Hello");
+                if (nodePathManager.pathID == 1)
+                {
+                    allowPath = true;
+                    lastPathID = nodePathManager.pathID;
+                }
+
+
+            }
+        }
+
+        else if (Physics.Raycast(transform.position + Vector3.up, transform.TransformDirection(Vector3.back), out backHit, 1.5f))
+        {
+
+            if (backHit.transform.CompareTag(turnRightPath))
+            {
+                //Debug.Log("Hello");
+                if(nodePathManager.pathID == 1)
+                {
+                    allowPath = true;
+                    lastPathID = nodePathManager.pathID;
+                }
+
+
+            }
+        }
+    }
 }
