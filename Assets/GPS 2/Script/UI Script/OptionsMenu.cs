338a0b8eb3185a86f92255e98056bab3f7960f92
NeeZaam <0123490@kdu-online.com>
2019-11-10 18:13:43 +0800

    Updated Main menu and pause menu UI, slight bugfix for building 3.

--- Assets/GPS 2/Script/UI Script/OptionsMenu.cs
+++ Assets/GPS 2/Script/UI Script/OptionsMenu.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class OptionsMenu : MonoBehaviour
 {
-  public void SetMasterVolume(float volume)
+    private bool musicOn = true;
+    private bool SFXon = true;
+
+    public void SetMasterVolume(float volume)
     {
         Global.audiomanager.setMasterVolume(volume);
 
@@ -18,5 +21,32 @@ public class OptionsMenu : MonoBehaviour
     {
         Global.audiomanager.setSFXVolume(volume);
 
+    }
+
+    public void setMusicActive()
+    {
+        musicOn = !musicOn;
+
+        if (musicOn == false)
+        {
+            Global.audiomanager.setBGMVolume(0.0f);
+        }
+        else
+        {
+            Global.audiomanager.setBGMVolume(0.7f);
+        }
+    }
+    public void setSFXActive()
+    {
+        SFXon = !SFXon;
+
+        if (musicOn == false)
+        {
+            Global.audiomanager.setSFXVolume(0.0f);
+        }
+        else
+        {
+            Global.audiomanager.setSFXVolume(0.7f);
+        }
     }
 }
