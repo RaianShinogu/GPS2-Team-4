d26a8957cd352092bb983109be1b7431ac07aa65
kahou0120424 <0120424@kdu-online.com>
2019-12-10 18:36:06 +0800

    Option for testing in phone or pc

--- Assets/CameraRaycast.cs
+++ Assets/CameraRaycast.cs
@@ -11,6 +11,7 @@ public class CameraRaycast : MonoBehaviour
     Ray ray;
     RaycastHit hit;
     public Text test;
+    public bool isOnPc;
     void Start()
     {
         nodeUI = NodeUI.instance;
@@ -20,33 +21,72 @@ public class CameraRaycast : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if(isOnPc != true)
         {
-            Touch touch = Input.GetTouch(0);
-            ray = Camera.main.ScreenPointToRay(touch.position);
-            
-            //layerMask = ~layerMask;
-            if (Physics.Raycast(ray, out hit, 100.0f))
+            if (Input.GetMouseButtonDown(0))
             {
-                Debug.Log(hit.collider.name);
-                Debug.Log(EventSystem.current.IsPointerOverGameObject());
-                if (EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
-                {
-                    return;
-                }
-                else if(!hit.collider.CompareTag("Node"))
-                {
-                    nodeUI.HideBuildUI();
-                    nodeUI.ResetConfirmation();
-                    Destroy(Node.ghostContainer);
-                    Node.ghostContainer = null;
-                    Node.haveGhost = false;
-                    nodeUI.HideUpDemUI();
-                    //test.text = Input.GetTouch(0).position.ToString();
-                }
-            }
+                Touch touch = Input.GetTouch(0);
+                ray = Camera.main.ScreenPointToRay(touch.position);
 
-            Debug.DrawLine(ray.origin, hit.point, Color.red, 5.0f);
+
+                //layerMask = ~layerMask;
+                if (Physics.Raycast(ray, out hit, 100.0f))
+                {
+                    Debug.Log(hit.collider.name);
+                    Debug.Log(EventSystem.current.IsPointerOverGameObject());
+                    if (EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
+                    {
+                        return;
+                    }
+
+                    else if (!hit.collider.CompareTag("Node"))
+                    {
+                        nodeUI.HideBuildUI();
+                        nodeUI.ResetConfirmation();
+                        Destroy(Node.ghostContainer);
+                        Node.ghostContainer = null;
+                        Node.haveGhost = false;
+                        nodeUI.HideUpDemUI();
+                        //test.text = Input.GetTouch(0).position.ToString();
+                    }
+                }
+
+                Debug.DrawLine(ray.origin, hit.point, Color.red, 5.0f);
+            }
+            
         }
+        else
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+
+                //layerMask = ~layerMask;
+                if (Physics.Raycast(ray, out hit, 100.0f))
+                {
+                    Debug.Log(hit.collider.name);
+                    Debug.Log(EventSystem.current.IsPointerOverGameObject());
+                    if (EventSystem.current.IsPointerOverGameObject())
+                    {
+                        return;
+                    }
+
+                    else if (!hit.collider.CompareTag("Node"))
+                    {
+                        nodeUI.HideBuildUI();
+                        nodeUI.ResetConfirmation();
+                        Destroy(Node.ghostContainer);
+                        Node.ghostContainer = null;
+                        Node.haveGhost = false;
+                        nodeUI.HideUpDemUI();
+                        //test.text = Input.GetTouch(0).position.ToString();
+                    }
+                }
+
+                Debug.DrawLine(ray.origin, hit.point, Color.red, 5.0f);
+            }
+        }
+
     }
 }
