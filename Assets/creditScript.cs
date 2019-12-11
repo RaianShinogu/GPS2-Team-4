c60a54d7bbabd1d23f59225a2d9e62db10af75c7
kahou0120424 <0120424@kdu-online.com>
2019-12-12 03:23:15 +0800

    Finish Up Credit scene

---
+++ Assets/creditScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class creditScript : MonoBehaviour
+{
+    float countDown;
+    public float secondBetween;
+    public GameObject thankYouUI;
+    public GameObject theTeamUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        thankYouUI.SetActive(true);
+        theTeamUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(countDown >= secondBetween)
+        {
+            thankYouUI.SetActive(false);
+            theTeamUI.SetActive(true);
+        }
+
+         if(countDown >= secondBetween*2)
+        {
+            SceneManager.LoadScene("Main Menu");
+        }
+
+        countDown += Time.deltaTime;
+    }
+}
